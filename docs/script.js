cd1cdb927162232ee5e7bb0d3cc830d0619cc6e9
houdini <houdini>
2019-08-05 19:07:12 +0300

    ADD: some markup and basic scope

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,20 @@
-console.log('Hello');
+"use strict";
+
+(function(){
+    const list = [];
+    const listElement = document.querySelector('#list');
+    const inputElement = document.querySelector('#new-task');
+    const buttonElemnt = document.querySelector('#addbutton');
+    buttonElemnt.addEventListener('click', ()=> {
+        let inputValue = inputElement.value;
+        if(inputElement.value.length < 1) {
+            return ;
+        }
+        list.push(inputElement.value);
+        inputElement.value = '';
+        console.log(list)
+    });
+
+
+
+})();
--- script.js
+++ script.js
@@ -5,3 +5,4 @@ app.use('/', express.static(__dirname + '/docs'));
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
+console.log('Server succesfully start on http://localhost:5000/');
